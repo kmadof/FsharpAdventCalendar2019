80c7367963fa62f1a1935fc9ca72f8f9282963ad
Krzysztof Madej <krzysztof.madej@hotmail.com>
2019-12-28 04:03:30 +0100

    Changed nuget references

--- build.fsx
+++ build.fsx
@@ -1,11 +1,10 @@
 #r "paket:
-source ../FAKE/src/app/Fake.Sql.DacPac/bin/Debug
-source https://api.nuget.org/v3/index.json
-nuget Fake.IO.FileSystem
-nuget Fake.Dotnet.MSBuild
-nuget Fake.Sql.SqlPackage
-nuget Fake.BuildServer.TeamCity
-nuget Fake.Core.Target"
+source https://api.nuget.org/v3/index.json //
+nuget Fake.IO.FileSystem //
+nuget Fake.Dotnet.MSBuild //
+nuget Fake.Sql.SqlPackage //
+nuget Fake.BuildServer.TeamCity //
+nuget Fake.Core.Target //" 
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core.TargetOperators
